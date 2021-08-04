d96bb3c5cce267018b6feb93452bdfbe2fdc7691
[theanujsharma] <[theanujsharma@outlook.com]>
2021-08-04 12:48:33 +0530

    Commit

---
+++ 3_August/script.js
@@ -0,0 +1,50 @@
+// Primitive Data Types
+// Strings "", '', and ''''''
+// Numbers 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, and 11
+// Boolean true and false
+
+//Composite Data Types | Collection Data Types
+// Arrays [] | List []
+// const a = [1,2,3,4,5]
+// console.log(a)
+// //Methods
+// a.pop() //Returns the last element of the array and removes it
+// console.log(a)
+// a.push(6) //Adds an element to the end of the array)
+// console.log(a)
+// a.shift() //Returns the first element of the array and removes it
+// console.log(a)
+// a.unshift(7) //Adds an element to the beginning of the array)
+// console.log(a)
+
+// const heading = document.querySelector("h1")
+// heading.remove()
+
+//function => A function is a block of code that is used to perform a specific task.
+// const num = document.querySelector("#num")
+// const btn = document.querySelector("#btn")
+// const table = document.querySelector("#table")
+// var value = num.value
+// console.log(value)
+// var newValue = parseInt(value)
+// console.log(newValue)
+
+// btn.addEventListener("click", 
+// function myFunction() {
+//   for (var i = 1; i < 10; i++) {
+//     console.log(i*value)
+//   }
+// })
+// myFunction(13) //arguments
+// myFunction(14) 
+// myFunction(16) //
+
+// var fname = num.value
+// btn.addEventListener("click",
+// function(){
+//   console.log(fname);
+// })
+
+let number = "2"
+console.log(typeof number) //strings
+console.log(typeof parseInt(number))    //numbers
