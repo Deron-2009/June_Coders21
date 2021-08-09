2d70aa50f36480858ddb7187c1e9134ed740f03b
[theanujsharma] <[theanujsharma@outlook.com]>
2021-08-09 21:19:45 +0530

    commit

---
+++ 9_August/script.js
@@ -0,0 +1,39 @@
+// let, var, and const keywords
+// let age = 32
+// let firstName = "John"
+// let isStudent = true // is or has
+// let students = ["alice", "bob", "charlie"]
+// let students = {
+//     alice:"32",
+//     bob:"33",
+//     charlie:"34"
+// }
+// let $t_3 = 7 // $, and _ are allowed in variable names
+// let johnAlice = "32" //camelCase
+const menu = {
+    "breakfast": "eggs",
+    "lunch": "spaghetti",
+    "dinner": "pizza",
+    food: function add(){
+        console.log("Hello")
+    },
+    students:["Deron", "John", "Chad", "Sara", "Kelsey", "Dennis"]
+}
+console.log(menu.food) // dot notation
+
+
+// const section = document.getElementsByTagName("div")
+// // console.log(section)
+// // for (let i = 0; i < section.length; i++) {
+// //     console.log(section[i].textContent)
+// // }
+// function greeting(fname){
+//     console.log("Hello " + fname)
+// }
+// // greeting("Yazir")
+// console.log(32) //function, already created by JS
+
+// .pop() //remove last element
+// .push() //add element to the end
+// .shift() //remove first element
+// .unshift() //add element to the beginning
